459d61bf5964ca2dd5ce5da58f8f5bb7c7eeb5a8
Milena Alcantara <milena.lima.alcantara61@aluno.ifce.edu.br>
2022-10-03 22:31:54 -0300

    learning animations

---
+++ animations/animations/animationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  animationsApp.swift
+//  animations
+//
+//  Created by Milena Lima de Alcântara on 02/10/22.
+//
+
+import SwiftUI
+
+@main
+struct animationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
